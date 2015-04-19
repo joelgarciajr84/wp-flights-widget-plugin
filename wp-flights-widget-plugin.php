2a2206ce93a390499f95d4b460d6dc64c962445a
joelgarciajr84 <joel.garciajr84@gmail.com>
2015-04-18 23:00:48 -0300

    First Commit

---
+++ wp-flights-widget-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WordPress Flights Widget Plugin
+ * Plugin URI: http://www.twitter.com/joelgarciajr84
+ * Description: Search flights on WordPress Widget
+ * Author: @joelgarciajr84
+ * Author URI: http://www.twitter.com/joelgarciajr84
+ * Version: 1.0
+ * License: GPLv2 or later
+ * Text Domain: wfwp
+ * Domain Path: languages/
+ */
